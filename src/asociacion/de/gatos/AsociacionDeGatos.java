a70f1309f1960a9784ade2501e3fcb439b9d3fa3
UCA <UCA@DEILAB02-PC8>
2018-04-23 15:38:32 -0600

    Jerry murio ese dia. Nadie supo que hacer. Solo recordaremos los ojos rojos satanicos que nos observan todas las noches desde ese día.

---
+++ src/asociacion/de/gatos/AsociacionDeGatos.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asociacion.de.gatos;
+
+/**
+ *
+ * @author UCA
+ */
+public class AsociacionDeGatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Gato gato1 = new Gato("Jerry");
+        Gato gato2 = new Gato("Alma de Jerry","exotico",2,"gris","rojo SATANICO","mediano");
+        System.out.println("La ID de "+gato1.getNombre()+ " es " + gato1.getID());
+        gato1.maullar();
+        gato1.morir();
+        System.out.println("");
+        gato2.maullar();
+        System.out.println(gato2.getNombre()+": Era "+gato1.getNombre()+" mi raza era "+gato2.getRaza()+" mis ojos ahora son "+gato2.getColorOjos());
+    }
+    
+}
---
+++ src/asociacion/de/gatos/Gato.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asociacion.de.gatos;
+
+/**
+ *
+ * @author UCA
+ */
+public class Gato {
+    public String nombre;
+    public String raza;
+    private int ID;
+    public int edad;
+    public String colorPelaje;
+    public String colorOjos;
+    public String size;
+    
+    public Gato(String nombre, String raza, int edad, String colorPelaje, String colorOjos, String size){
+        this.nombre=nombre;
+        this.raza=raza;
+        this.edad=edad;
+        this.colorPelaje=colorPelaje;
+        this.colorOjos=colorOjos;
+        this.size=size;
+        
+    }
+    
+    public Gato(){
+    
+    }
+    
+    public Gato(String nombre){
+        this.nombre=nombre;
+        setID();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID() {
+        this.ID = (int)(1000000*Math.random());   
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getColorPelaje() {
+        return colorPelaje;
+    }
+
+    public void setColorPelaje(String colorPelaje) {
+        this.colorPelaje = colorPelaje;
+    }
+
+    public String getColorOjos() {
+        return colorOjos;
+    }
+
+    public void setColorOjos(String colorOjos) {
+        this.colorOjos = colorOjos;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+    
+    
+    //Cosas que puede hacer el gato (?????)
+    
+    public void maullar(){
+        System.out.println(getNombre()+": Miau");
+    }
+    
+    public void morir(){
+        System.out.println(getNombre()+" ha fallecido");
+    }
+    
+    
+}
